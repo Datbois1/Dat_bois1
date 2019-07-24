1c3559ec02b296a250f29fc5993740de303b70dd
BuildTools <unconfigured@null.spigotmc.org>
2019-07-24 15:13:56 -0700

    :/

--- Game.py
+++ Game.py
@@ -1,36 +1,23 @@
 import pygame
-pygame.init
+from bullet import *
+pygame.init()
 Screensize = (width, height) = (800,600)
 screen = pygame.display.set_mode(Screensize)
 color = (165, 42, 42)
 clock = pygame.time.Clock()
-Bullet = pygame.image.load("bullet.png")
-Bullet = pygame.transform.scale(Bullet, (100, 50))
-bullet_rect = Bullet.get_rect()
-speed = pygame.math.Vector2(10, 10)
 screeninfo = pygame.display.Info()
+bullet = bullet()
+
+
 def main():
     while True:
         clock.tick(60)
-        Move()
+        bullet.Move()
         screen.fill(color)
-        screen.blit(Bullet, bullet_rect)
+        bullet.draw(screen)
         pygame.display.flip()
-def Move():
-    bullet_rect.move_ip(speed)
-    width = screeninfo.current_w
-    if bullet_rect.right > width:
-        speed.x *= -1
-    if bullet_rect.left < 0:
-        speed.x  *=-1
-    if bullet_rect.top < height:
-        speed.y *=-1
-    if bullet_rect.bottom > 0:
-        speed.y *=-1
 
 
-
-
-if __name__=="__main__":
+if __name__== "__main__":
     main()
 
--- bullet.py
+++ bullet.py
@@ -1,6 +1,26 @@
+import pygame
+
+
 class bullet:
-    def __init__(self, pos):
-    self.image = pygame.image.load("bullet.png")
-    self.image = pygame.transform.scale(self.image, (100, 50))
-    self.rect = Bullet.get_rect()
-    self.speed = pygame.math.Vector2(10, 10)
+    def __init__(self):
+        self.image = pygame.image.load("bullet.png")
+        self.image = pygame.transform.scale(self.image, (100, 50))
+        self.rect = self.image.get_rect()
+        self.speed = pygame.math.Vector2(10, 10)
+
+
+    def Move(self):
+        screeninfo = pygame.display.Info()
+        self.rect.move_ip(self.speed)
+        width = screeninfo.current_w
+        height = screeninfo.current_h
+        if self.rect.right > width:
+            self.speed.x *= -1
+        if self.rect.left < 0:
+            self.speed.x *= -1
+        if self.rect.top < height:
+            self.speed.y *= -1
+        if self.rect.bottom > 0:
+            self.speed.y *= -1
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
